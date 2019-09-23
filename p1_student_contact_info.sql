bcdd2b26be55d36e2fea0bf26c5a87f1b0508e24
YukiKakegawa <kake.19940627@gmail.com>
2019-09-23 09:32:51 -0600

    with_to_subquery

--- p1_student_contact_info.sql
+++ p1_student_contact_info.sql
@@ -1,4 +1,4 @@
--- Personal Information Path1
+-- With Statement version
 WITH email AS (
     SELECT person_id, contact_number 
     FROM person_contact 
@@ -35,4 +35,29 @@ FROM
     LEFT JOIN email e ON (e.person_id = s.person_id)  -- 5: email
     LEFT JOIN cell_number cn ON (cn.person_id = s.person_id)  -- 1: cell 
     LEFT JOIN home_phone hp ON (hp.person_id = s.person_id) -- 2: home
+    LEFT JOIN PERSON_ADDRESS pa ON S.PERSON_ID = PA.PERSON_ID;
+
+
+-- Subquery version
+SELECT DISTINCT 
+     s.PERSON_ID
+   , e.CONTACT_NUMBER email
+   , cn.CONTACT_NUMBER phone1
+   , hp.CONTACT_NUMBER phone
+   , pa.ADDRESS1
+   , pa.ADDRESS2
+   , pa.ADDRESS3
+   , pa.ADDRESS4
+   , pa.postal_code 
+   , pa.country_id
+   , s.do_not_contact
+    
+FROM 
+    STUDENT s
+    LEFT JOIN (SELECT person_id, contact_number FROM person_contact WHERE contact_type_id = 5) e 
+        ON (e.person_id = s.person_id)  -- 5: email
+    LEFT JOIN (SELECT person_id, contact_number FROM person_contact WHERE contact_type_id = 1) cn 
+        ON (cn.person_id = s.person_id)  -- 1: cell 
+    LEFT JOIN (SELECT person_id, contact_number FROM person_contact WHERE contact_type_id = 2) hp 
+        ON (hp.person_id = s.person_id) -- 2: home
     LEFT JOIN PERSON_ADDRESS pa ON S.PERSON_ID = PA.PERSON_ID;
